04f5b241a31e9d27db32ee0c01e63c306bc5cab4
Matthew Allison <matthewallisonxyz@gmail.com>
2024-03-01 20:39:02 +0200

    Basic structure of recipe class
    Begun creation of basic recipe class structure.

---
+++ Classes/Recipe.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatthewAllison_st10269378_PROG6221_POE1.Classes
+{
+    internal class Recipe 
+    {
+        private struct Ingredient
+        {
+            private string name;
+            private int quantity;
+            private UnitOfMeasurement unit; 
+
+        }
+
+        public enum UnitOfMeasurement
+        {
+            table_spoon,
+            grams,
+            ounces,
+            
+        }
+
+        private struct Step
+        {
+            private int position;
+            private string description;
+        }
+
+
+
+        int Num_Ingredients;
+        int Num_Setps;
+        Step[]? steps;
+        Ingredient[]? ingredients;
+
+
+
+    }
+
+}
