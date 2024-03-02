26ee2dbe26e5becbf7abaf1c6ca89a719728c49d
Matthew Allison <matthewallisonxyz@gmail.com>
2024-03-02 12:41:37 +0200

    Improved text output
    Greatly improved the text output of the user interface for a much better user experience.

--- Classes/UserInterface.cs
+++ Classes/UserInterface.cs
@@ -22,7 +22,7 @@ namespace MatthewAllison_st10269378_PROG6221_POE1.Classes
                 string choice = "0";
 
                 Console.WriteLine("RECIPE PROCESSING SOFTWARE");
-                Console.WriteLine("");
+                Console.WriteLine();
                 Console.WriteLine("1. Create New Recipe");
                 Console.WriteLine("2. View Existing Recipe");
                 Console.WriteLine("3. Change Current Recipe");
@@ -31,6 +31,7 @@ namespace MatthewAllison_st10269378_PROG6221_POE1.Classes
                 Console.WriteLine("");
                 Console.WriteLine("Enter choice: ");
                 choice = Console.ReadLine();
+                Console.WriteLine();
 
                 switch (choice)
                 {
@@ -56,77 +57,86 @@ namespace MatthewAllison_st10269378_PROG6221_POE1.Classes
         private void DeleteRecipe()
         {
             recipe = new Recipe();
-            CreateRecipe();
+            Console.WriteLine("Recipe Deleted Successfully");
+            Console.WriteLine();
         }
 
         private void ChangeRecipe()
         {
             Console.WriteLine("Adjusting Recipe Scale");
             Console.WriteLine("");
-            Console.WriteLine($"Current scaling factor is {recipe.Scaling_factor()}");
+            Console.WriteLine($"Current scaling factor is: {recipe.Scaling_factor()}");
             Console.WriteLine();
             Console.WriteLine("1. Adjust scale");
             Console.WriteLine("2. Reset scael");
+            Console.WriteLine("Enter choice: ");
             string choice = Console.ReadLine();
+            Console.WriteLine();
             switch (choice)
             {
                 case "1":
                     Console.WriteLine("Enter new scaling factor: ");
                     recipe.Scaling_factor(float.Parse(Console.ReadLine()));
-                    Console.WriteLine($"Scaling factor adjusted to {recipe.Scaling_factor()}");
+                    Console.WriteLine($"Scaling factor adjusted to: {recipe.Scaling_factor()}");
                     break;
                 case "2":
-                    Console.WriteLine("Scaling factor reset to 1");
+                    Console.WriteLine("Scaling factor reset to: 1");
                     recipe.Scaling_factor(1);
                     break;
             }
+            Console.WriteLine();
         }
 
         private void ViewRecipe()
         {
             Console.WriteLine($"RECIPE: {recipe.Name()}");
-            Console.WriteLine($"Number of Ingredients: {recipe.ingredients.Length}");
+            Console.WriteLine($"Number of ingredients: {recipe.ingredients.Length}");
             Console.WriteLine($"Number of steps: {recipe.steps.Length}");
+            Console.WriteLine();
             Console.WriteLine("LIST OF INGREDIENTS: ");
+
             for ( int i = 0; i < recipe.ingredients.Length; i++ )
             {
-                Console.Write($"{i}. ");
+                Console.Write($"{i + 1}. ");
                 Console.WriteLine(recipe.ingredients[i].ToString(recipe.Scaling_factor()));
             }
-            Console.WriteLine("");
+            Console.WriteLine();
+
             Console.WriteLine("LIST OF STEPS: ");
             for ( int i = 0; i < recipe.steps.Length; i++ )
             {
-                Console.Write($"{i}. ");
+                Console.Write($"{i + 1}. ");
                 Console.WriteLine(recipe.steps[i].Description());
             }
-            Console.WriteLine("");
-            Console.WriteLine("End of recipe.");
+            
+            Console.WriteLine();
+            Console.WriteLine("END OF RECIPE.");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            Console.WriteLine();
         }
 
         private void CreateRecipe()
         {
             Console.WriteLine("Enter recipe name: ");
-            try
-            {
-                recipe.Name(Console.ReadLine());
-            }
-            catch
-            {
-                Console.WriteLine("");
-            }
+            recipe.Name(Console.ReadLine());
+            Console.WriteLine();
 
             Console.WriteLine("Enter number of ingredients: ");
             int num_ingredients = int.Parse(Console.ReadLine());
             recipe.MakeIngriedientsArray(num_ingredients);
+            Console.WriteLine();
 
             for (int i = 0; i < recipe.ingredients.Length; i++)
             {
-                Console.WriteLine("Ingriedient Name: ");
+                Console.WriteLine($"Ingredient {i + 1}:");
+                Console.WriteLine("Enter ingriedient name: ");
                 recipe.ingredients[i].Name = Console.ReadLine();
+                Console.WriteLine() ;
 
-                Console.WriteLine("Measurement Unit: ");
-                Console.WriteLine("Enter one of the following: ");
+                Console.WriteLine("MEASUREMENT UNITS: ");
+                Console.WriteLine("-------------------");
 
                 foreach (
                     Recipe.CookingMeasurement measurement in Enum.GetValues(
@@ -136,6 +146,9 @@ namespace MatthewAllison_st10269378_PROG6221_POE1.Classes
                 {
                     Console.WriteLine(measurement);
                 }
+                Console.WriteLine("-------------------");
+
+                Console.WriteLine("Enter one of the above: ");
                 string input = Console.ReadLine();
 
                 if (
@@ -148,18 +161,23 @@ namespace MatthewAllison_st10269378_PROG6221_POE1.Classes
                 {
                     recipe.ingredients[i].Unit = unit;
                 }
-                Console.WriteLine("Quantity: ");
+                Console.WriteLine();
+
+                Console.WriteLine("Enter quanity: ");
                 recipe.ingredients[i].Quantity = int.Parse(Console.ReadLine());
+                Console.WriteLine();
             }
             Console.WriteLine("Thank you, all ingredients captured");
+            Console.WriteLine();
 
             Console.WriteLine("Enter number of steps: ");
             int num_steps = int.Parse(Console.ReadLine());
             recipe.MakeStepsArray(num_steps);
+            Console.WriteLine();
 
             for (int i = 0; i < num_steps; i++)
             {
-                Console.WriteLine($"Please enter step {i}");
+                Console.WriteLine($"Please enter step {i + 1}");
                 Recipe.Step step = new Recipe.Step();
                 step.Position(i);
                 step.Description(Console.ReadLine());
@@ -167,6 +185,7 @@ namespace MatthewAllison_st10269378_PROG6221_POE1.Classes
                 recipe.steps[i] = step;
             }
             Console.WriteLine("Thank you. Recipe has been captured");
+            Console.WriteLine();
         }
     }
 }
