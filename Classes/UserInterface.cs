6d99270610eb689ebb33a240c96affea16f17c34
Matthew Allison <matthewallisonxyz@gmail.com>
2024-03-02 11:30:42 +0200

    Delete Recipe and Change Recipe
    Implemented the delete recipe method and part implemented the view recipe method.

--- Classes/UserInterface.cs
+++ Classes/UserInterface.cs
@@ -17,40 +17,80 @@ namespace MatthewAllison_st10269378_PROG6221_POE1.Classes
 
         public void MainMenu()
         {
-            string choice = "0";
-            while (choice == "0")
+            while (true)
             {
+                string choice = "0";
+
                 Console.WriteLine("RECIPE PROCESSING SOFTWARE");
                 Console.WriteLine("");
                 Console.WriteLine("1. Create New Recipe");
                 Console.WriteLine("2. View Existing Recipe");
                 Console.WriteLine("3. Change Current Recipe");
+                Console.WriteLine("4. Delete Existing Recipe");
+                Console.WriteLine("5. Exit");
                 Console.WriteLine("");
                 Console.WriteLine("Enter choice: ");
                 choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        CreateRecipe();
+                        break;
+                    case "2":
+                        ViewRecipe();
+                        break;
+                    case "3":
+                        ChangeRecipe();
+                        break;
+                    case "4":
+                        DeleteRecipe();
+                        break;
+                    default:
+                        Environment.Exit(0);
+                        break;
+                }
             }
-            switch (choice)
-            {
-                case "1":
-                    CreateRecipe();
-                    break;
-                case "2":
-                    ViewRecipe();
-                    break;
-                case "3":
-                    ChangeRecipe();
-                    break;
-            }
+        }
+
+        private void DeleteRecipe()
+        {
+            recipe = new Recipe();
+            CreateRecipe();
         }
 
         private void ChangeRecipe()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Adjusting Recipe Scale");
+            Console.WriteLine("");
+            Console.WriteLine($"Current scaling factor is {recipe.Scaling_factor()}");
+            Console.WriteLine();
+            Console.WriteLine("1. Adjust scale");
+            Console.WriteLine("2. Reset scael");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Enter new scaling factor: ");
+                    float.Parse(Console.ReadLine());
+                    Console.WriteLine($"Scaling factor adjusted to {recipe.Scaling_factor()}");
+                    break;
+                case "2":
+                    Console.WriteLine("Scaling factor reset to 1");
+                    break;
+            }
         }
 
         private void ViewRecipe()
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"RECIPE: {recipe.Name()}");
+            Console.WriteLine($"Number of Ingredients: {recipe.ingredients.Length}");
+            Console.WriteLine($"Number of steps: {recipe.steps.Length}");
+            Console.WriteLine("LIST OF INGREDIENTS: ");
+            for ( int i = 0; i < recipe.ingredients.Length; i++ )
+            {
+                Console.WriteLine(recipe.ingredients[i]);
+            }
         }
 
         private void CreateRecipe()
@@ -69,7 +109,6 @@ namespace MatthewAllison_st10269378_PROG6221_POE1.Classes
             int num_ingredients = int.Parse(Console.ReadLine());
             recipe.MakeIngriedientsArray(num_ingredients);
 
-            
             for (int i = 0; i < recipe.ingredients.Length; i++)
             {
                 Console.WriteLine("Ingriedient Name: ");
@@ -107,17 +146,15 @@ namespace MatthewAllison_st10269378_PROG6221_POE1.Classes
             int num_steps = int.Parse(Console.ReadLine());
             recipe.MakeStepsArray(num_steps);
 
-            for (int i = 0;i < num_steps; i++)
+            for (int i = 0; i < num_steps; i++)
             {
                 Console.WriteLine($"Please enter step {i}");
                 Recipe.Step step = new Recipe.Step();
                 step.Position(i);
                 step.Description(Console.ReadLine());
                 Console.WriteLine();
-
             }
             Console.WriteLine("Thank you. Recipe has been captured");
-
         }
     }
 }
